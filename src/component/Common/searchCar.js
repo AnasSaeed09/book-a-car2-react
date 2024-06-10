e3b8a39b8ee476fe2d8b378806b1885bbc4c218e
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2024-06-10 23:13:31 +0500

    Update searchCar.js

--- src/component/Common/searchCar.js
+++ src/component/Common/searchCar.js
@@ -1,12 +1,13 @@
 import {React} from 'react'
-import about from '../../images/about.jpg'
+import { Link } from 'react-router-dom'
+
 export default function SearchCar() {
   return (
    <>
     <section className="ftco-section ftco-about">
 			<div className="container">
 				<div className="row no-gutters">
-					<div className="col-md-6 p-md-5 img img-2 d-flex justify-content-center align-items-center" style={{backgroundImage: (about)}}>
+					<div className="col-md-6 p-md-5 img img-2 d-flex justify-content-center align-items-center" style={{backgroundImage: `url('../../images/about.jpg')`}}>
 					</div>
 					<div className="col-md-6 wrap-about ftco-animate">
 	          <div className="heading-section heading-section-white pl-md-5">
@@ -15,7 +16,7 @@ export default function SearchCar() {
 
 	            <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
 	            <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-	            <p><link href="#" className="btn btn-primary py-3 px-4">Search Vehicle</link></p>
+	            <p><Link To="#" className="btn btn-primary py-3 px-4">Search Vehicle</Link></p>
 	          </div>
 					</div>
 				</div>
