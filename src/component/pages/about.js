089c759fd46b50cb4c5146a9632d858c43b154c6
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2024-06-10 23:18:19 +0500

    Update about.js

--- src/component/pages/about.js
+++ src/component/pages/about.js
@@ -5,18 +5,19 @@ import BecomeAdriverBtn from '../Common/becomeAdriverBtn'
 import Testimony from '../Common/testimony'
 import CarCount from '../Common/CarCount'
 import Footer from '../Common/footer'
+import { Link } from 'react-router-dom'
 
 export default function About() {
   return (
     <>
      <Header/>
-     <section class="hero-wrap hero-wrap-2 js-fullheight" style={{backgroundImage: ('images/bg_3.jpg')}} data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-          <div class="col-md-9 ftco-animate pb-5">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
-            <h1 class="mb-3 bread">About Us</h1>
+     <section className="hero-wrap hero-wrap-2 js-fullheight" style={{backgroundImage: ('images/bg_3.jpg')}} data-stellar-background-ratio="0.5">
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+          <div className="col-md-9 ftco-animate pb-5">
+          	<p className="breadcrumbs"><span className="mr-2"><Link To="index.html">Home <i className="ion-ios-arrow-forward"></i></Link></span> <span>About us <i className="ion-ios-arrow-forward"></i></span></p>
+            <h1 className="mb-3 bread">About Us</h1>
           </div>
         </div>
       </div>
