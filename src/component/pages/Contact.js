24da1e02c6e11df97dc997bcded5a1234872fd2f
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2024-06-10 23:15:49 +0500

    Update Contact.js

--- src/component/pages/Contact.js
+++ src/component/pages/Contact.js
@@ -1,78 +1,79 @@
 import React from 'react'
 import Header from '../Common/header'
 import Footer from '../Common/footer'
+import { Link } from 'react-router-dom'
 
 export default function Contact() {
   return (
     <>
    
-     <section class="hero-wrap hero-wrap-2 js-fullheight" style={{backgroundImage: `url('../../images/bg_3.jpg')`}} data-stellar-background-ratio="0.5">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
-          <div class="col-md-9 ftco-animate pb-5">
-          	<p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Contact <i class="ion-ios-arrow-forward"></i></span></p>
-            <h1 class="mb-3 bread">Contact Us</h1>
+     <section className="hero-wrap hero-wrap-2 js-fullheight" style={{backgroundImage: `url('../../images/bg_3.jpg')`}} data-stellar-background-ratio="0.5">
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="row no-gutters slider-text js-fullheight align-items-end justify-content-start">
+          <div className="col-md-9 ftco-animate pb-5">
+          	<p className="breadcrumbs"><span className="mr-2"><Link To="index.html">Home <i className="ion-ios-arrow-forward"></i></Link></span> <span>Contact <i className="ion-ios-arrow-forward"></i></span></p>
+            <h1 className="mb-3 bread">Contact Us</h1>
           </div>
         </div>
       </div>
     </section>
 
-    <section class="ftco-section contact-section">
-      <div class="container">
-        <div class="row d-flex mb-5 contact-info">
-        	<div class="col-md-4">
-        		<div class="row mb-5">
-		          <div class="col-md-12">
-		          	<div class="border w-100 p-4 rounded mb-2 d-flex">
-			          	<div class="icon mr-3">
-			          		<span class="icon-map-o"></span>
+    <section className="ftco-section contact-section">
+      <div className="container">
+        <div className="row d-flex mb-5 contact-info">
+        	<div className="col-md-4">
+        		<div className="row mb-5">
+		          <div className="col-md-12">
+		          	<div className="border w-100 p-4 rounded mb-2 d-flex">
+			          	<div className="icon mr-3">
+			          		<span className="icon-map-o"></span>
 			          	</div>
 			            <p><span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016</p>
 			          </div>
 		          </div>
-		          <div class="col-md-12">
-		          	<div class="border w-100 p-4 rounded mb-2 d-flex">
-			          	<div class="icon mr-3">
-			          		<span class="icon-mobile-phone"></span>
+		          <div className="col-md-12">
+		          	<div className="border w-100 p-4 rounded mb-2 d-flex">
+			          	<div className="icon mr-3">
+			          		<span className="icon-mobile-phone"></span>
 			          	</div>
-			            <p><span>Phone:</span> <a href="tel://1234567920">+ 1235 2355 98</a></p>
+			            <p><span>Phone:</span> <Link To="tel://1234567920">+ 1235 2355 98</Link></p>
 			          </div>
 		          </div>
-		          <div class="col-md-12">
-		          	<div class="border w-100 p-4 rounded mb-2 d-flex">
-			          	<div class="icon mr-3">
-			          		<span class="icon-envelope-o"></span>
+		          <div className="col-md-12">
+		          	<div className="border w-100 p-4 rounded mb-2 d-flex">
+			          	<div className="icon mr-3">
+			          		<span className="icon-envelope-o"></span>
 			          	</div>
-			            <p><span>Email:</span> <a href="mailto:info@yoursite.com">info@yoursite.com</a></p>
+			            <p><span>Email:</span> <Link To="mailto:info@yoursite.com">info@yoursite.com</Link></p>
 			          </div>
 		          </div>
 		        </div>
           </div>
-          <div class="col-md-8 block-9 mb-md-5">
-            <form action="#" class="bg-light p-5 contact-form">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Name"/>
+          <div className="col-md-8 block-9 mb-md-5">
+            <form action="#" className="bg-light p-5 contact-form">
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Your Name"/>
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Your Email"/>
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Your Email"/>
               </div>
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="Subject"/>
+              <div className="form-group">
+                <input type="text" className="form-control" placeholder="Subject"/>
               </div>
-              <div class="form-group">
-                <textarea name="" id="" cols="30" rows="7" class="form-control" placeholder="Message"></textarea>
+              <div className="form-group">
+                <textarea name="" id="" cols="30" rows="7" className="form-control" placeholder="Message"/>
               </div>
-              <div class="form-group">
-                <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5"/>
+              <div className="form-group">
+                <input type="submit" value="Send Message" className="btn btn-primary py-3 px-5"/>
               </div>
             </form>
           
           </div>
         </div>
-        <div class="row justify-content-center">
-        	<div class="col-md-12">
-        		<div id="map" class="bg-white"></div>
+        <div className="row justify-content-center">
+        	<div className="col-md-12">
+        		<div id="map" className="bg-white"></div>
         	</div>
         </div>
       </div>
