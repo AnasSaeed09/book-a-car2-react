ba2dbcedd0467aa66ddfbde4b1d95df372531ad2
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2024-06-10 23:12:06 +0500

    Update becomeAdriverBtn.js

--- src/component/Common/becomeAdriverBtn.js
+++ src/component/Common/becomeAdriverBtn.js
@@ -1,15 +1,16 @@
 import React from 'react'
-import bg_3 from '../../images/bg_3.jpg'
+import { Link } from 'react-router-dom'
+
 export default function BecomeAdriverBtn() {
   return (
     <>
-    <section className="ftco-section ftco-intro" style={{backgroundImage:(bg_3)}}>
+    <section className="ftco-section ftco-intro" style={{backgroundImage:`url(../../images/bg_3.jpg)`}}>
 			<div className="overlay"></div>
 			<div className="container">
 				<div className="row justify-content-end">
 					<div className="col-md-6 heading-section heading-section-white ftco-animate">
             <h2 className="mb-3">Do You Want To Earn With Us? So Don't Be Late.</h2>
-            <link href="#" className="btn btn-primary btn-lg">Become A Driver</link>
+            <Link To="#" className="btn btn-primary btn-lg">Become A Driver</Link>
           </div>
 				</div>
 			</div>
