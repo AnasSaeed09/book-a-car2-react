a5842f530de01033ed668b17ff9838be7d781cf6
Anas <169180642+AnasSaeed09@users.noreply.github.com>
2024-06-10 23:12:52 +0500

    Update header.js

--- src/component/Common/header.js
+++ src/component/Common/header.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+const Wrapper =styled.div.attrs({
+  className:'login-logo'
+})`
+*{
+color:white;
+font-weight:bolder;
 
+}
+
+.login-logo{
+position:relative;
+background:linear-gradient(90deg,#928fcc, #190c7c);
+width:50px;
+height:30px;
+text-align:center;
+border-radius:35px;
+top:10px;
+}
+.login-logo span{
+background:linear-gradient(90deg,red,blue);
+border-radius:35px;
+
+}
+.login:hover{
+color:yellow;
+}
+.login:active{
+color:black;
+}
+`
 export default function Header() {
   return (
-    <>
+    <Wrapper>
       <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
         <div className="container">
           <Link className="navbar-brand" to="/">Car<span>Book</span></Link>
@@ -20,10 +50,17 @@ export default function Header() {
               <li className="nav-item"><Link to="/car" className="nav-link">Cars</Link></li>
               <li className="nav-item"><Link to="/blog" className="nav-link">Blog</Link></li>
               <li className="nav-item"><Link to="/contact" className="nav-link">Contact</Link></li>
+               <li className="nav-item">
+               <div className='login-logo'>
+                <span><Link to="/Login" className='login'>Login</Link></span>
+              </div>
+               </li>
+
             </ul>
           </div>
         </div>
       </nav>
-    </>
+     
+    </Wrapper>
   );
 }
